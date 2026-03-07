ccec845b193104133d63b29515335a55216124fa
Mark Trevor Birss <markbirss@gmail.com>
2026-03-08 00:45:18 +0200

    Update ST7789Spi.h

--- src/ST7789Spi.h
+++ src/ST7789Spi.h
@@ -379,8 +379,9 @@ class ST7789Spi : public OLEDDisplay {
         // Source EQ Enable
         sendCommand(0xB7);
         WriteData(0x13);
-        sendCommand(ST77XX_SLPOUT); //  2: Out of sleep mode, no args, w/delay
-        delay(10);
+      
+        //sendCommand(ST77XX_SLPOUT); //  2: Out of sleep mode, no args, w/delay
+        //delay(10);
 
         // Gate Line Setting - 400 line
         sendCommand(0xB0);
@@ -390,6 +391,7 @@ class ST7789Spi : public OLEDDisplay {
         sendCommand(0x3A);
         WriteData(0x11);
         delay(200);
+      
         // Gamma Mode Setting - Mono
         sendCommand(0xB9);
         WriteData(0x20);
@@ -397,12 +399,9 @@ class ST7789Spi : public OLEDDisplay {
         sendCommand(ST77XX_MADCTL); //  4: Mem access ctrl (directions)
         WriteData(_MADCTL); 
 
-        sendCommand(0xBB); // Enable Clear RAM
-        WriteData(0xFF);  // CLR=0 ; Enable Clear RAM,clear RAM to 0
-
         sendCommand(ST77XX_CASET); //   5: Column addr set,
         WriteData(0x12);
-        WriteData(0x2B);
+        WriteData(0x2A);
       
         sendCommand(ST77XX_RASET); //   6: Row addr set,    
         WriteData(0x00);
@@ -431,14 +430,11 @@ class ST7789Spi : public OLEDDisplay {
         sendCommand(0xD0);
         WriteData(0xFF);
       
-        sendCommand(ST77XX_NORON); //  8: Normal display on, no args, w/delay
-        delay(10);
-        
-        sendCommand(ST77XX_DISPON); //  9: Main screen turn on, no args, delay
-        delay(10);
-      
         sendCommand(0x38); // HPM:How Power Mode ON
-
+      
+        sendCommand(0xBB); // Enable Clear RAM
+        WriteData(0xFF);  // CLR=0 ; Enable Clear RAM,clear RAM to 0
+      
         sendCommand(0x29); // DISPLAY ON
       
         delay(10);
