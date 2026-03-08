f84f90e9204248f9a98b6613e698aa5156c51e93
Mark Trevor Birss <markbirss@gmail.com>
2026-03-08 02:26:18 +0200

    Update ST7789Spi.h

--- src/ST7789Spi.h
+++ src/ST7789Spi.h
@@ -381,8 +381,9 @@ class ST7789Spi : public OLEDDisplay {
         WriteData(0x13);
       
         // Gate Line Setting - 400 line
-        //sendCommand(0xB0);
+        sendCommand(0xB0);
         //WriteData(0x64);
+        WriteData(0x60);
 
         sendCommand(ST77XX_SLPOUT); //  2: Out of sleep mode, no args, w/delay
         delay(120);
